9c28f9285a4d2b032d39a73dad884bf2310c4dad
opalenet-farzad <fkamali@opale.net>
2020-03-27 17:15:37 -0400

    base

---
+++ components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <ul>
+            <li><a>ce que nous faisons</a></li>
+            <li><a>portfolio</a></li>
+            <li><a>à propos de nous</a></li>
+            <li><a>contactez-nous</a></li>
+            <li><a>En</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <ul>
+            <li><a>instagram</a></li>
+            <li><a>linkedin</a></li>
+          </ul>
+        </div>
+      </div>
+      <p>Copyright © 2015 - 2020 All rights reserved</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+@import 'assets/style/footer.css';
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<style>
+@import 'assets/style/general.css';
+</style>
+<script>
+import Footer from '../components/Footer'
+export default {
+  components: { Footer }
+}
+</script>
